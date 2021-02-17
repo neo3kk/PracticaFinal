d60d81d7ffac7e1fbbf769c8fae7744aea0e2543
Jlopez <neo3kk@gmail.com>
2021-02-18 00:38:12 +0100

    updateGitignore

---
+++ BackEnd/src/main/java/com/rest/vue/VueApplication.java
@@ -0,0 +1,28 @@
+package com.rest.vue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class VueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VueApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8080")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+}
